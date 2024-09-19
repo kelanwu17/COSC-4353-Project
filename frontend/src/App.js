4c41a3de1eb183ebfc6dc73bc4d31b90894b0143
Christopher Saucedo <slapdaneck2003@gmail.com>
2024-09-19 15:35:25 -0500

    Edited home page back to normal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,11 @@ import LogInPage from './Pages/Login/LogIn';
 import HomePage from './Pages/Home/Home';
 
 function App() {
-  return (
-    <div className="App">
-       <HomePage/>
-    </div>
-  );
+ return ( 
+      <HomePage/>
+        
+    )
+
 }
 
 export default App;
