da9ca7cadf26b29c6eec4e026df416b5ef0992a1
Osvaldo Mosso <osvaldomosso@Osvaldos-MacBook-Air-2.local>
2024-09-19 14:37:05 -0500

    added user profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
-
 import './App.css';
 import UserProfile from './Pages/User Profile/UserProfile';
+import UserDisplay from './Pages/User Profile/UserDisplay';
 
-function App() {
+
+
+ function App() {
   return (
     <div className="App">
-       
+      <UserProfile/>
+      <UserDisplay/>
     </div>
   );
 }
