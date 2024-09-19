64f41da653d9c708e4375b3f694f7e464c53bb9b
pykroth <pyrakroth@gmail.com>
2024-09-19 14:38:33 -0500

    resolved merge conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,30 +5,15 @@ import LogInPage from './Pages/Login/LogIn';
 import HomePage from './Pages/Home/Home';
 
 function App() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 22d4a3bfac04ad7ef8e3a8a1621ab81c5237fda0
     return ( <
         div className = "App" >
 
         <
+        HomePage / >
+        <
         /div>
-    );
+    )
+
 }
 
 export default App;
-<<<<<<< HEAD
-=======
-  return (
-    <div className="App">
-       <HomePage/>
-    </div>
-  );
-}
-
-export default App;
-
->>>>>>> 84bb4d364169174134aa6a2b3289b7094bbfe966
-=======
->>>>>>> 22d4a3bfac04ad7ef8e3a8a1621ab81c5237fda0
