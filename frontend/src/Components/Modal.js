ace099a6bd3fa3af59c5a12b5d4bf07e6395d7cc
pykroth <pyrakroth@gmail.com>
2024-09-20 18:01:52 -0500

    fixed all indentions

--- frontend/src/Components/Modal.js
+++ frontend/src/Components/Modal.js
@@ -28,19 +28,23 @@ const overlayStyle = {
 };
 
 
-export default function Modal({ open, children, onClose, title, description, urgency, skills, location, date }) {
+export default function Modal({ open, children, onClose, title, description, urgency, skills, location, date}) {
     const [isTrueReg, setIsTrueReg] = useState(false);
     const modalRef = useRef(null);
-
-    function handleReg(e) {
-        e.stopPropagation();
+    
+    function handleReg(e)
+    {
+        e.stopPropagation(); 
         onClose();
-        if (isTrueReg == false) {
-            setIsTrueReg(true)
-        } else {
-            setIsTrueReg(false)
-        }
-
+            if (isTrueReg == false)
+            {
+                setIsTrueReg(true)
+            }
+            else
+            {
+                setIsTrueReg(false)
+            }
+            
     }
 
     useEffect(() => {
@@ -61,42 +65,27 @@ export default function Modal({ open, children, onClose, title, description, urg
 
     if (!open) return null;
 
-    return ReactDOM.createPortal( <
-        div style = { overlayStyle } >
-        <
-        div style = { modalStyle }
-        ref = { modalRef }
-        className = 'bg-gray-400' >
-
-        <
-        div className = "text-xl text-bold mb-2" > { title } < /div> <
-        div className = "text-xs" > Location: { location }, Date: { date } < /div> <
-        div style = {
-            { overflowWrap: 'break-word', wordBreak: 'break-word' } } > { description } < /div> <
-        div > Urgency: { urgency } < /div> <
-        div style = {
-            { overflowWrap: 'break-word', wordBreak: 'break-word' } } > Skills Required: {
-            skills.map((item, index) => ( <
-                span style = {
-                    { overflowWrap: 'break-word', wordBreak: 'break-word' } }
-                key = { index } > { item } < /span>
-            ))
-        } <
-        /div>
-
-        <
-        div > Date: { date } < /div> <
-        button className = "rounded-sm border border-black bg-blue-400 text-white absolute bottom-2 right-18 "
-        onClick = {
-            (e) => { e.stopPropagation();
-                onClose(); } } > Close < /button> <
-        button className = "rounded-sm border border-black bg-blue-400 text-white absolute bottom-2 right-2"
-        onClick = { handleReg } > Register < /button>
+    return ReactDOM.createPortal(
+        <div style={overlayStyle} >
+            <div style={modalStyle} ref={modalRef} className='bg-gray-400'>
+                
+                <div className="text-xl text-bold mb-2">{title}</div>
+                <div className="text-xs">Location: {location}, Date: {date}</div>
+                <div style={{ overflowWrap: 'break-word', wordBreak: 'break-word' }}>{description}</div>
+                <div>Urgency: {urgency}</div>
+                <div style={{ overflowWrap: 'break-word', wordBreak: 'break-word' }}>Skills Required: 
+                    {skills.map((item, index) => ( 
+                        <span style={{ overflowWrap: 'break-word', wordBreak: 'break-word' }} key={index}>{item}</span>
+                    ))}
+                    </div>
+                
+                <div>Date: {date}</div>
+                <button className="rounded-sm border border-black bg-blue-400 text-white absolute bottom-2 right-18 " onClick={(e) => { e.stopPropagation(); onClose(); }}>Close</button>
+                <button className="rounded-sm border border-black bg-blue-400 text-white absolute bottom-2 right-2" onClick={handleReg}>Register</button>
 
 
-        <
-        /div> <
-        /div>,
+            </div>
+        </div>,
         document.getElementById('portal')
     );
 }
